7bd597bb59e50c03b9ac3fe608aafa74a0fdbe5f
m-zebrak <mateusz.zebrak@gmail.com>
2021-07-15 00:38:28 +0200

    new events added

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,3 +1,4 @@
+from itertools import cycle
 from math import floor
 import random
 import sys
@@ -6,17 +7,23 @@ import pygame as pg
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREY = (64, 64, 64)
 
 # Game settings
-WIDTH = 640
-HEIGHT = 480
-MENU_HEIGHT = 50
+WIDTH = 1024
+HEIGHT = 576
+MENU_HEIGHT = 40
 TITLE = 'conway\'s game of life'
 ICON = 'icon.ico'
+CELL_SIZES = cycle([5, 10, 16, 32, 40])
+
+# Mouse buttons
+LMB = 0
+RMB = 2
 
 
 class GameOfLife:
-    def __init__(self, cell_size=10, max_fps=20):
+    def __init__(self, cell_size=15, max_fps=20):
         """
         Initialize screen, initialize grid, set game settings, draw first frame
 
@@ -34,20 +41,33 @@ class GameOfLife:
         self.max_fps = 1 if max_fps < 1 else max_fps
 
         self.new()
+        self.paused = False
+        self.show_grid = True
 
-    def new(self):
+    def new(self, randomize: int = 0):
+        if randomize == 1:
+            last_width = self.grid_width
+            last_height = self.grid_height
+
         self.grid_width = int(WIDTH / self.cell_size)
         self.grid_height = int((HEIGHT - MENU_HEIGHT) / self.cell_size)
 
-        print(f'num of cols: {self.grid_width}, num of rows: {self.grid_height}')
+        if randomize == 0:
+            self.grid = [[0] * self.grid_height for _ in range(self.grid_width)]
+            self.fill_grid()
+        elif randomize == 1:
+            self.grid_width = int(WIDTH / self.cell_size)
+            self.grid_height = int((HEIGHT - MENU_HEIGHT) / self.cell_size)
 
-        self.grid = [[0] * self.grid_height for _ in range(self.grid_width)]
-        self.fill_grid()
+            temp = [[0] * self.grid_height for _ in range(self.grid_width)]
+
+            for c in range(last_width):
+                for r in range(last_height):
+                    temp[c][r] = self.grid[c][r]
+            self.grid = temp
 
         self.generation = 0
         self.alive_cells = 0
-        self.paused = False
-        self.exit = False
 
     def fill_grid(self, value=None):
         """
@@ -58,22 +78,44 @@ class GameOfLife:
 
         :param value:  Value to set the cell to (0 or 1)
         """
+        self.generation = 0
+        self.alive_cells = 0
         self.grid = [[random.choice([0, 1]) if value is None else value for x in self.grid[0]] for y in self.grid]
 
-    def draw_grid(self):
+    def draw_grid(self, mid_width, mid_height):
+        width = self.grid_width * self.cell_size + mid_width
+        height = self.grid_height * self.cell_size + mid_height
+
+        for x in range(0, width, self.cell_size):
+            new_x = x + mid_width
+            pg.draw.line(self.screen, GREY, (new_x, mid_height), (new_x, height))
+        for y in range(0, height, self.cell_size):
+            new_y = y + mid_height
+            pg.draw.line(self.screen, GREY, (mid_width, new_y), (width, new_y))
+
+    def draw(self):
         """
         Draw the cells from active grid on the screen
         """
         self.alive_cells = 0
-        for c in range(self.grid_width):
-            for r in range(self.grid_height):
-                if self.grid[c][r] == 1:
+        mid_width = int((WIDTH - self.grid_width * self.cell_size) / 2)
+        mid_height = int((HEIGHT - MENU_HEIGHT - self.grid_height * self.cell_size) / 2)
+
+        pg.draw.rect(self.screen, WHITE, (0, 0, WIDTH, HEIGHT - MENU_HEIGHT))
+
+        for x in range(0, self.grid_width):
+            for y in range(0, self.grid_height):
+                if self.grid[x][y] == 1:
                     self.alive_cells += 1
                     color = BLACK
                 else:
                     color = WHITE
                 pg.draw.rect(self.screen, color,
-                             (c * self.cell_size, r * self.cell_size, self.cell_size, self.cell_size))
+                             (x * self.cell_size + mid_width, y * self.cell_size + mid_height, self.cell_size,
+                              self.cell_size))
+
+        if self.show_grid:
+            self.draw_grid(mid_width, mid_height)
         pg.display.flip()
 
     def count_cell_neighbors(self, col, row):
@@ -127,9 +169,6 @@ class GameOfLife:
         """
         Displaying information about generation and alive cells in the bottom
         """
-        print(f'Generation: {self.generation}')
-        print(f'Alive cells: {self.alive_cells}')
-
         div = 1
 
         while True:
@@ -168,17 +207,24 @@ class GameOfLife:
         :return: None if clicked below grid otherwise tuple (col, row)
         """
         # only if clicked above menu bar (on the grid)
-        if pos[1] < (HEIGHT - MENU_HEIGHT):
+        if pos[0] < (self.grid_width * self.cell_size) and pos[1] < (self.grid_height * self.cell_size):
             return floor(pos[0] / self.cell_size), floor(pos[1] / self.cell_size)
         return None
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
 
     def handle_events(self):
         """
         Handle key presses
-
+        g - toggle on/off grid
         p - toggle start/stop (pause) the game
-        r - randomize grid
+        t - switch between cell sizes
+        z - decrease cell size
+        x - increase cell size
         n - display next generation
+        r - randomize grid
         c - clear grid
         q - quit
         LMB - pressed or held sets cell alive
@@ -186,80 +232,80 @@ class GameOfLife:
         """
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                pg.quit()
-                sys.exit()
-            elif event.type == pg.KEYDOWN:
+                self.quit()
+            if event.type == pg.KEYDOWN:
                 if event.key == pg.K_p:
                     print("'p' pressed! - toggling pause")
                     self.paused = not self.paused
+                    return
                 elif event.key == pg.K_q:
                     print("q pressed! - quitting the game")
-                    self.exit = True
+                    self.quit()
                 elif event.key == pg.K_r:
                     print("'r' pressed! - randomizing grid")
-                    self.generation = 0
-                    self.alive_cells = 0
                     self.fill_grid()
-                    self.draw_grid()
-                    self.display_info()
                 elif self.paused and event.key == pg.K_n:
                     print("'n' pressed! - displaying next generation")
                     self.update_generation()
-                    self.draw_grid()
-                    self.display_info()
                 elif self.paused and event.key == pg.K_c:
                     print("'c' pressed! - clearing active grid")
-                    self.generation = 0
-                    self.alive_cells = 0
                     self.fill_grid(0)
-                    self.draw_grid()
-                    self.display_info()
+                elif event.key == pg.K_t:
+                    print("'t' pressed! - changing cell size")
+                    self.cell_size = next(CELL_SIZES)
+                    self.new()
+                elif event.key == pg.K_g:
+                    print("'g' pressed! - toggling grid")
+                    self.show_grid = not self.show_grid
+                elif event.key == pg.K_x:
+                    print("'x' pressed! - cell size increased")
+                    self.cell_size += 2 if self.cell_size < 100 else 0
+                    self.new(randomize=2)
+                elif event.key == pg.K_z:
+                    print("'z' pressed! - cell size decreased")
+                    self.cell_size -= 2 if self.cell_size > 5 else 0
+                    self.new(randomize=1)
             elif pg.mouse.get_pressed():
+                col_row = None
+                click = pg.mouse.get_pressed() or pg.mouse.get_pressed()
+
                 try:
-                    col_row = None
-                    # LMB [0] or RMB [2] pressed
-                    if pg.mouse.get_pressed()[0] or pg.mouse.get_pressed()[2]:
-                        col_row = self.get_col_row_by_mouse(event.pos)
-
-                    if col_row is not None:
-                        col, row = col_row
-
-                        if pg.mouse.get_pressed()[0]:
-                            if self.grid[col][row] == 1:
-                                return
-                            self.grid[col][row] = 1
-                        elif pg.mouse.get_pressed()[2]:
-                            if self.grid[col][row] == 0:
-                                return
-                            self.grid[col][row] = 0
-
-                        self.draw_grid()
-                        self.display_info()
-                # when the mouse is pressed down and moved out of the window
-                except AttributeError:
+                    col_row = self.get_col_row_by_mouse(event.pos)
+                except AttributeError:  # when the mouse is pressed down and moved out of the window
                     pass
+
+                if col_row is None:
+                    continue
+
+                col, row = col_row
+                cell = self.grid[col][row]
+
+                if click[LMB] and cell == 0:
+                    self.grid[col][row] = 1
+                elif click[RMB] and cell == 1:
+                    self.grid[col][row] = 0
+                else:
+                    continue
+
+            self.draw()
+            self.display_info()
 
     def run(self):
         """
         Starts the game and loops until the quit state
         """
         while True:
-            if self.exit:
-                pg.quit()
-                return
-
             self.handle_events()
 
             if not self.paused:
                 update = True
-
-                self.draw_grid()
+                self.draw()
                 self.display_info()
                 self.update_generation()
 
                 # when paused - the user is able to set or delete cells by mouse with more fps
                 self.clock.tick(self.max_fps)
             elif update:
-                self.draw_grid()
+                self.draw()
                 self.display_info()
                 update = False
